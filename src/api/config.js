b9086b8ee6a48333c4fca2505af02a6d853a97dc
White-paper <530366891@qq.com>
2018-08-14 15:45:02 +0800

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.modifyEnv = modifyEnv;
-var PREFIX_URL = exports.PREFIX_URL = 'https://qzone.melonblock.com/fans';
-var UAID = exports.UAID = 60006;
+var PREFIX_URL = exports.PREFIX_URL = '';
+var UAID = exports.UAID = 0;
 var QINIU_PREFIX = exports.QINIU_PREFIX = '-xingkong'; //七牛云储存前缀，防误删
 var SUCCESS_CODE = exports.SUCCESS_CODE = 200;
 var TRADE_TYPE = exports.TRADE_TYPE = 'MWEB'; // h5交易类型
