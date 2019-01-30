344e07b9489bead074b5552f0c085877f8f2edbb
luoyukun <530366891@qq.com>
2019-01-30 16:39:42 +0800

    增加config 文件

---
+++ src/api/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+// let PREFIX_URL = exports.PREFIX_URL = 'http://localhost:3000'
+let PREFIX_URL = exports.PREFIX_URL = 'http://node.jiangzhifan.com';
+let QINIU_PREFIX = exports.QINIU_PREFIX = 'web-card-qiNiu' // 七牛云储存前缀，防误删
+let UAID = exports.UAID = 1000
